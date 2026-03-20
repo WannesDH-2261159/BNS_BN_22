86da1fe840b34f91ef5ae7de9a3061dde43bbb02
Alessio Ieracitano <115924711+AlessioI-2261404@users.noreply.github.com>
2026-03-20 09:42:47 +0100

    Cleaned up main.py code

--- Botnet/main.py
+++ Botnet/main.py
@@ -5,23 +5,42 @@ from pyshortcuts import make_shortcut
 import os
 import sys
 
+def check_shortcut_exists():
+    shortcut_path = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows',
+                                 'Start Menu', 'Programs', 'Startup', 'BotnetStartup.lnk')
+    return os.path.exists(shortcut_path)
+
+def create_shortcut():
+    destination = os.path.join(os.path.join(os.environ['USERPROFILE']), 'AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup')
+    make_shortcut(sys.argv[0], name="BotnetStartup", folder=destination)
+
+def ask_for_startup_shortcut():
+    ans = notification(0, "Botnet want to run at startup.\n\nadd to startup apps?", "Botnet notification", 4)
+    if ans == 6:
+        print("OK")
+        create_shortcut()
+
+def ask_to_become_bot():
+    ans = notification(0, "Running this software will make you part of a botnet.\n\nRun botnet software?", "Botnet notification", 4)
+    if ans == 6:
+        return True
+    elif ans == 7:
+        return False
+
+def handle_startup_bot():
+    if not check_shortcut_exists():
+        ask_for_startup_shortcut()
+    
+    test = Bot()
+
+
+# BOT STARTUP EXECUTION CODE
 if __name__ == "__main__":
     notification = ctypes.windll.user32.MessageBoxW
     notification.restype = ctypes.c_int
 
-    ans = notification(0, "You are part of a botnet.\nRun botnet software?", "Botnet notification", 4)
-    if ans == 6:
-        print("OK")
-        # Ask to create startup shortcut
-        ans = notification(0, "You are part of a botnet.\nAdd to startup apps?", "Botnet notification", 4)
-        if ans == 6:
-            print("OK")
-            destination = os.path.join(os.path.join(os.environ['USERPROFILE']), 'AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup')
-            make_shortcut(sys.argv[0], name="BotnetStartup", folder=destination)
-        
-        # Start bot
-        test = Bot()
-    
-    elif ans == 7:
-        print("No")
-    
+    becomeBot = ask_to_become_bot()
+    if becomeBot:
+        handle_startup_bot()
+    else:
+        sys.exit(0)
